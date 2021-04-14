d1fd7bd32a9a0bed33f8b33a1aaba758b4df49c8
archanashokeeniitg <archanashokeeniitg@gmail.com>
2021-04-14 01:36:13 -0700

    trying n testing

--- src/views/Services.js
+++ src/views/Services.js
@@ -73,7 +73,6 @@ export default function Services() {
             alt="name"
           />
           <GridListTileBar
-            style={{ fontWeight: "bold", fontSize: 3 }}
             title="Organic Farming"
             actionIcon={
               <IconButton aria-label={`info about `} className={classes.icon}>
